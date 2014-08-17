873d357a0029eb92c8dd8c424cf95f280d3e8ec9
Gizmo <earldbjr@gmail.com>
2014-08-17 01:31:15 -0400

    Computer side transmit working using strings, any length. Com selector 0-9 working.

--- AI2Main.cpp
+++ AI2Main.cpp
@@ -10,15 +10,20 @@
 #include "wx_pch.h"
 #include "AI2Main.h"
 #include <wx/msgdlg.h>
-
+#include <termios.h>
+#include <fcntl.h>
+int fd; //Port to operate on
+int PORT; //To be set by scPortNumber
 //(*InternalHeaders(AI2Frame)
 #include <wx/string.h>
 #include <wx/intl.h>
 //*)
 
 //helper functions
-enum wxbuildinfoformat {
-    short_f, long_f };
+enum wxbuildinfoformat
+{
+    short_f, long_f
+};
 
 wxString wxbuildinfo(wxbuildinfoformat format)
 {
@@ -43,6 +48,13 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(AI2Frame)
+const long AI2Frame::ID_STATICTEXT2 = wxNewId();
+const long AI2Frame::ID_SPINCTRL1 = wxNewId();
+const long AI2Frame::ID_TOGGLEBUTTON1 = wxNewId();
+const long AI2Frame::ID_BUTTON1 = wxNewId();
+const long AI2Frame::ID_BUTTON2 = wxNewId();
+const long AI2Frame::ID_STATICTEXT1 = wxNewId();
+const long AI2Frame::ID_PANEL1 = wxNewId();
 const long AI2Frame::idMenuQuit = wxNewId();
 const long AI2Frame::idMenuAbout = wxNewId();
 const long AI2Frame::ID_STATUSBAR1 = wxNewId();
@@ -58,11 +70,34 @@ AI2Frame::AI2Frame(wxWindow* parent,wxWindowID id)
     //(*Initialize(AI2Frame)
     wxMenuItem* MenuItem2;
     wxMenuItem* MenuItem1;
+    wxFlexGridSizer* FlexGridSizer1;
     wxMenu* Menu1;
+    wxBoxSizer* BoxSizer1;
     wxMenuBar* MenuBar1;
     wxMenu* Menu2;
 
     Create(parent, id, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("id"));
+    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
+    Panel1 = new wxPanel(this, ID_PANEL1, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL1"));
+    FlexGridSizer1 = new wxFlexGridSizer(0, 3, 0, 0);
+    lblPortNumber = new wxStaticText(Panel1, ID_STATICTEXT2, _("Port Number:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
+    FlexGridSizer1->Add(lblPortNumber, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    scPortNumber = new wxSpinCtrl(Panel1, ID_SPINCTRL1, _T("0"), wxDefaultPosition, wxSize(50,27), 0, 0, 9, 0, _T("ID_SPINCTRL1"));
+    scPortNumber->SetValue(_T("0"));
+    FlexGridSizer1->Add(scPortNumber, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    btnTogglePort = new wxToggleButton(Panel1, ID_TOGGLEBUTTON1, _("Closed"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TOGGLEBUTTON1"));
+    FlexGridSizer1->Add(btnTogglePort, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    btnTest1 = new wxButton(Panel1, ID_BUTTON1, _("Test1"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
+    FlexGridSizer1->Add(btnTest1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    btnTest2 = new wxButton(Panel1, ID_BUTTON2, _("Test2"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
+    FlexGridSizer1->Add(btnTest2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    lblResponse = new wxStaticText(Panel1, ID_STATICTEXT1, _("Not Yet Implemented"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
+    FlexGridSizer1->Add(lblResponse, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Panel1->SetSizer(FlexGridSizer1);
+    FlexGridSizer1->Fit(Panel1);
+    FlexGridSizer1->SetSizeHints(Panel1);
+    BoxSizer1->Add(Panel1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    SetSizer(BoxSizer1);
     MenuBar1 = new wxMenuBar();
     Menu1 = new wxMenu();
     MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
@@ -79,7 +114,13 @@ AI2Frame::AI2Frame(wxWindow* parent,wxWindowID id)
     StatusBar1->SetFieldsCount(1,__wxStatusBarWidths_1);
     StatusBar1->SetStatusStyles(1,__wxStatusBarStyles_1);
     SetStatusBar(StatusBar1);
+    BoxSizer1->Fit(this);
+    BoxSizer1->SetSizeHints(this);
 
+    Connect(ID_SPINCTRL1,wxEVT_COMMAND_SPINCTRL_UPDATED,(wxObjectEventFunction)&AI2Frame::OnscPortNumberChange);
+    Connect(ID_TOGGLEBUTTON1,wxEVT_COMMAND_TOGGLEBUTTON_CLICKED,(wxObjectEventFunction)&AI2Frame::OnbtnTogglePortToggle);
+    Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&AI2Frame::OnbtnTest1Click);
+    Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&AI2Frame::OnbtnTest2Click);
     Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&AI2Frame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&AI2Frame::OnAbout);
     //*)
@@ -100,4 +141,72 @@ void AI2Frame::OnAbout(wxCommandEvent& event)
 {
     wxString msg = wxbuildinfo(long_f);
     wxMessageBox(msg, _("Welcome to..."));
+}
+
+void transmit(std::string buffer)
+{
+
+    char message[buffer.size()+1]; //+1 to contain buffer + a null terminating char
+    strncpy(message, buffer.c_str(), sizeof(message));
+    message[sizeof(message) - 1] = 0; // forces the last character to have a null termination
+    write(fd, &message, sizeof(message));//-1 = fail
+}
+void AI2Frame::OnbtnTogglePortToggle(wxCommandEvent& event)
+{
+    if(btnTogglePort->GetValue() == 1)
+    {
+        btnTogglePort->SetLabel(wxT("Open"));
+        char chPortName[13]="/dev/ttyACM0";
+        sprintf(chPortName, "/dev/ttyACM%i", PORT);
+
+        int fd = open(chPortName, O_RDWR | O_NOCTTY | O_NDELAY);
+
+        struct termios config;
+
+        if(tcgetattr(fd, &config) < 0)
+        {
+            //  cout << "tcgetattr: fd < 0";
+        }
+
+        config.c_iflag  &= ~(IGNBRK | BRKINT  | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
+        //~(Turn off processing | Convert break to null byte | No CR->NL | No NL->CR | Ignore Parity Errors | Dont strip high bit | no flow control);
+        config.c_oflag = 0;
+        config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+        config.c_cflag &= ~(CSIZE | PARENB);
+        config.c_cflag  |= CS8;
+        config.c_cc[VMIN] = 1;
+        config.c_cc[VTIME] = 0;
+        if(cfsetispeed(&config, B9600)  < 0 || cfsetospeed(&config, B9600) < 0)
+        {
+            //  cout << "Problem setting Baud to 9600";
+        }
+        if(tcsetattr(fd, TCSAFLUSH, &config) < 0)
+        {
+            //  cout << "Problem setting attributes to fd";
+        }
+    }
+    if(btnTogglePort->GetValue() == 0)
+    {
+        btnTogglePort->SetLabel(wxT("Closed"));
+        close(fd);
+    }
+}
+
+void AI2Frame::OnscPortNumberChange(wxSpinEvent& event)
+{
+    btnTogglePort->SetValue(0);
+    btnTogglePort->SetLabel(wxT("Closed"));
+    close(fd);
+PORT = scPortNumber->GetValue();
+}
+
+void AI2Frame::OnbtnTest1Click(wxCommandEvent& event)
+{
+    std::string data = "12345";
+    transmit(data);
+}
+
+void AI2Frame::OnbtnTest2Click(wxCommandEvent& event)
+{
+    transmit("12346");
 }
--- AI2Main.h
+++ AI2Main.h
@@ -11,9 +11,15 @@
 #define AI2MAIN_H
 
 //(*Headers(AI2Frame)
+#include <wx/spinctrl.h>
+#include <wx/sizer.h>
+#include <wx/button.h>
 #include <wx/menu.h>
+#include <wx/panel.h>
 #include <wx/statusbr.h>
 #include <wx/frame.h>
+#include <wx/tglbtn.h>
+#include <wx/stattext.h>
 //*)
 
 class AI2Frame: public wxFrame
@@ -28,16 +34,34 @@ class AI2Frame: public wxFrame
         //(*Handlers(AI2Frame)
         void OnQuit(wxCommandEvent& event);
         void OnAbout(wxCommandEvent& event);
+        void OnbtnTogglePortToggle(wxCommandEvent& event);
+        void OnscPortNumberChange(wxSpinEvent& event);
+        void OnbtnTest1Click(wxCommandEvent& event);
+        void OnbtnTest2Click(wxCommandEvent& event);
         //*)
 
         //(*Identifiers(AI2Frame)
+        static const long ID_STATICTEXT2;
+        static const long ID_SPINCTRL1;
+        static const long ID_TOGGLEBUTTON1;
+        static const long ID_BUTTON1;
+        static const long ID_BUTTON2;
+        static const long ID_STATICTEXT1;
+        static const long ID_PANEL1;
         static const long idMenuQuit;
         static const long idMenuAbout;
         static const long ID_STATUSBAR1;
         //*)
 
         //(*Declarations(AI2Frame)
+        wxSpinCtrl* scPortNumber;
+        wxPanel* Panel1;
+        wxToggleButton* btnTogglePort;
+        wxButton* btnTest2;
         wxStatusBar* StatusBar1;
+        wxButton* btnTest1;
+        wxStaticText* lblPortNumber;
+        wxStaticText* lblResponse;
         //*)
 
         DECLARE_EVENT_TABLE()
